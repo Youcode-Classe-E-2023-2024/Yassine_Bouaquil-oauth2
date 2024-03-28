fcc663cd12b181babc4b399e2dab4ba84314c1c9
Yassine Bouaquil <139541292+yassine-bouaquil@users.noreply.github.com>
2024-03-28 09:49:28 +0000

    MAIN

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,5 +25,15 @@ Route::post('login', 'AuthController@login');
 Route::post('register', 'AuthController@register');
 Route::middleware('auth:api')->group(function () {
     Route::get('user', 'AuthController@user');
-    // Other authenticated routes...
+
 });
+
+
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::get('/users', [UserController::class, 'index']);
+Route::post('/users', [UserController::class, 'store']);
+Route::get('/users/{user}', [UserController::class, 'show']);
+Route::put('/users/{user}', [UserController::class, 'update']);
+Route::delete('/users/{user}', [UserController::class, 'destroy']);
